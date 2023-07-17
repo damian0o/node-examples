5580d1d984a2dd53ff4c8f6febe68a8004ba17f9
Damian0o <damian@arapso.pl>
2023-07-17 17:38:26 +0200

    Combine logs from all nodes

--- node-web/pm2-express-basic-cluster/README.md
+++ node-web/pm2-express-basic-cluster/README.md
@@ -2,4 +2,5 @@
 - PM2 should be installed (npm install pm2 -g)
 
 
-pm2 start
+    pm2 start
+    pm2 logs
--- node-web/pm2-express-basic-cluster/ecosystem.config.js
+++ node-web/pm2-express-basic-cluster/ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
         instances: 4,
         exec_mode: "cluster",
         watch: true,
+	log_file: "./express-cluster.log",
+	combine_logs: true,
         increment_var : 'PORT',
         env: {
             "PORT": 3000,
